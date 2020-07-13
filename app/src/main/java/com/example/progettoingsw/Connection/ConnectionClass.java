2e1d09bc2e36f0b308dec2556864d113fc29e34d
salvio96 <67631172+salvio96@users.noreply.github.com>
2020-07-13 15:58:53 +0200

    login_connessionedbms

---
+++ app/src/main/java/com/example/progettoingsw/Connection/ConnectionClass.java
@@ -0,0 +1,9 @@
+package com.example.progettoingsw.Connection;
+
+public class ConnectionClass {
+    public static String ip ="progettoserver.database.windows.net"; // SQL Server IP Address
+    public static String un= "adminSal"; // SQL Server User name
+    public static String pass = "SimSal1920"; // SQL Server Password
+    public static String db = "ProgettoINGSW"; // SQL Server Database
+
+}

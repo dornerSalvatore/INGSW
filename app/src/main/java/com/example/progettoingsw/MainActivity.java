f9e91c5bb3eff9b52dc617090f1f9b7fd5083eb3
salvio96 <67631172+salvio96@users.noreply.github.com>
2020-07-13 14:00:31 +0200

    login_connessionedbms

--- app/src/main/java/com/example/progettoingsw/MainActivity.java
+++ app/src/main/java/com/example/progettoingsw/MainActivity.java
@@ -1,60 +1,71 @@
 package com.example.progettoingsw;
 
+import java.sql.Connection;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.DialogFragment;
 import java.lang.Object;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.progettoingsw.Connection.ConnectionClass;
 
 
 public class MainActivity extends Activity {
+
+    Connection con;
+    EditText username;
+    EditText password;
+    Button bottone1;
+    Button bottone3;
+    Button bottone2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText myEditText = (EditText) findViewById(R.id.editTextTextPersonName);
-        myEditText.setText(myEditText.getText().toString());
-        Button bottone1=(Button) findViewById(R.id.bottone1);
+
+
+
+
+         username = (EditText) findViewById(R.id.editTextTextPersonName);
+        username.setText(username.getText().toString());
+        password = (EditText) findViewById(R.id.editTextTextPassword);
+        username.setText(username.getText().toString());
+        bottone1=(Button) findViewById(R.id.bottone1);
+
         bottone1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
-                dialog.setTitle("ERRORE");
-                dialog.setMessage("username non valido");
-                dialog.show();*/
-                Dialog dialog ;
-                //dialog = new Dialog ();
-                /*dialog.setContentView(R.layout.dialog1);
 
 
-                Button bottone2 = (Button) findViewById(R.id.button);
-                bottone2.setOnClickListener(new View.OnClickListener() {
-                    public void onClick(View v) {
-                        dialog.dismiss();
-                    }
-                });
-                dialog.show();*/
-                Button bottone5=(Button) findViewById(R.id.bottone1);
-              Button bottone2=(Button) findViewById(R.id.bottone1);
-                      bottone2.setOnClickListener(new View.OnClickListener() {
+                      bottone1.setOnClickListener(new View.OnClickListener() {
                           @Override
                           public void onClick(View view) {
-                              Intent openPage1 = new Intent(MainActivity.this,MainActivity2.class);
-                              // passo all'attivazione dell'activity Pagina.java
-                              startActivity(openPage1);
+                              new MainActivity.checkLogin().execute("");
                           }
                       });
             }
@@ -62,7 +73,7 @@ public class MainActivity extends Activity {
 
 
 
-        Button bottone3=(Button) findViewById(R.id.bottone3);
+        bottone3=(Button) findViewById(R.id.bottone3);
         bottone3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -72,7 +83,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        Button bottone2=(Button) findViewById(R.id.bottone2);
+         bottone2=(Button) findViewById(R.id.bottone2);
         bottone2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -83,4 +94,87 @@ public class MainActivity extends Activity {
         });
 
         };
+
+    public class checkLogin extends AsyncTask<String, String, String> {
+
+        String z = null;
+        Boolean isSuccess = false;
+
+
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            con =connectionClass(ConnectionClass.un.toString(),ConnectionClass.pass.toString(),ConnectionClass.db.toString(),ConnectionClass.ip.toString());
+            if(con == null){
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this,"Check Internet Connection",Toast.LENGTH_LONG).show();
+                    }
+                });
+                z = "On Internet Connection";
+            }
+            else {
+                try {
+                    String sql = "SELECT * FROM Utente WHERE username = '" + username.getText() + "' AND passwd= '" + password.getText() + "' ";
+                    Statement stmt = con.createStatement();
+                    ResultSet rs = stmt.executeQuery(sql);
+
+                    if (rs.next()) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_LONG);
+                            }
+                        });
+                        z = "Success";
+
+                        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "Check email or password", Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+                        username.setText("");
+                        password.setText("");
+                    }
+                } catch (Exception e) {
+                    isSuccess = false;
+                    Log.e("SQL Error : ", e.getMessage());
+                }
+            }
+            return z;
+        }
+    }
+    @SuppressLint("NewApi")
+    public Connection connectionClass(String user, String password, String database, String server){
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String connectionURL = null;
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            connectionURL = "jdbc:jtds:sqlserver://" + server+"/" + database + ";user=" + user + ";password=" + password + ";";
+            connection = DriverManager.getConnection(connectionURL);
+        }catch (Exception e){
+            Log.e("SQL Connection Error : ", e.getMessage());
+        }
+
+        return connection;
+    }
+
         }
